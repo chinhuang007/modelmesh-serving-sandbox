60cf59925e03e378e080a8d5413ce10afc0fe890
Chin Huang <chhuang@us.ibm.com>
2021-07-27 12:55:27 -0700

    s

---
+++ scripts/deploy/iks/undeploy-mm-serving.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Copyright 2021 kubeflow.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Remove the x if you need no print out of each command
+set -ex
+
+# Need the following env var
+# - SERVING_NS:                        namespace for modelmesh-serving, defulat: modelmesh-serving
+
+# These env vars should come from the build.properties that `build-image.sh` generates
+echo "PIPELINE_KUBERNETES_CLUSTER_NAME=${PIPELINE_KUBERNETES_CLUSTER_NAME}"
+echo "SERVING_NS=${SERVING_NS}"
+echo "MANIFEST=${MANIFEST}"
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "BUILD_NUMBER=${BUILD_NUMBER}"
+echo "ARCHIVE_DIR=${ARCHIVE_DIR}"
+echo "GIT_BRANCH=${GIT_BRANCH}"
+echo "GIT_COMMIT=${GIT_COMMIT}"
+echo "GIT_COMMIT_SHORT=${GIT_COMMIT_SHORT}"
+echo "REGION=${REGION}"
+echo "ORG=${ORG}"
+echo "SPACE=${SPACE}"
+echo "RESOURCE_GROUP=${RESOURCE_GROUP}"
+
+retry() {
+  local max=$1; shift
+  local interval=$1; shift
+
+  until "$@"; do
+    echo "trying.."
+    max=$((max-1))
+    if [[ "$max" -eq 0 ]]; then
+      return 1
+    fi
+    sleep "$interval"
+  done
+}
+
+retry 3 3 ibmcloud login --apikey "${IBM_CLOUD_API_KEY}" --no-region
+retry 3 3 ibmcloud target -r "$REGION" -o "$ORG" -s "$SPACE" -g "$RESOURCE_GROUP"
+retry 3 3 ibmcloud ks cluster config -c "$PIPELINE_KUBERNETES_CLUSTER_NAME"
+
+./scripts/delete.sh
+
+kubectl delete ns "$SERVING_NS"
+
+echo "Finished modelmesh-serving undeployment." 
+
