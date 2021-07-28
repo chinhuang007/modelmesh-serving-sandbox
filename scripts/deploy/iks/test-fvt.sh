6055dbff122a45c921ebee250cf44cda04be0650
Chin Huang <chhuang@us.ibm.com>
2021-07-27 17:16:31 -0700

    s

--- scripts/deploy/iks/test-fvt.sh
+++ scripts/deploy/iks/test-fvt.sh
@@ -14,6 +14,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Remove the x if you need no print out of each command
+set -ex
+
+# These env vars should come from the build.properties that `build-image.sh` generates
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "BUILD_NUMBER=${BUILD_NUMBER}"
+echo "ARCHIVE_DIR=${ARCHIVE_DIR}"
+echo "GIT_BRANCH=${GIT_BRANCH}"
+echo "GIT_COMMIT=${GIT_COMMIT}"
+echo "GIT_COMMIT_SHORT=${GIT_COMMIT_SHORT}"
+echo "REGION=${REGION}"
+echo "ORG=${ORG}"
+echo "SPACE=${SPACE}"
+echo "RESOURCE_GROUP=${RESOURCE_GROUP}"
+
+retry() {
+  local max=$1; shift
+  local interval=$1; shift
+
+  until "$@"; do
+    echo "trying.."
+    max=$((max-1))
+    if [[ "$max" -eq 0 ]]; then
+      return 1
+    fi
+    sleep "$interval"
+  done
+}
+
 # fvt
 run_flip_coin_example() {
   local REV=1
@@ -24,7 +54,7 @@ run_flip_coin_example() {
   #python3 samples/flip-coin/condition.py
   #retry 3 3 kfp --endpoint http://localhost:8888 pipeline upload -p e2e-flip-coin samples/flip-coin/condition.yaml || :
   go test -v ./fvt -ginkgo.v -ginkgo.progress -test.timeout 40m
-  
+
   REV=0
   if [[ "$REV" -eq 0 ]]; then
     echo " =====   flip coin sample PASSED ====="
@@ -34,6 +64,10 @@ run_flip_coin_example() {
 
   return "$REV"
 }
+
+retry 3 3 ibmcloud login --apikey "${IBM_CLOUD_API_KEY}" --no-region
+retry 3 3 ibmcloud target -r "$REGION" -o "$ORG" -s "$SPACE" -g "$RESOURCE_GROUP"
+retry 3 3 ibmcloud ks cluster config -c "$SERVING_KUBERNETES_CLUSTER_NAME"
 
 RESULT=0
 run_flip_coin_example 20 || RESULT=$?
