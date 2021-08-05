bd8dcd7840a30aeee2d57c26fc3c7335cce77e18
Chin Huang <chhuang@us.ibm.com>
2021-08-05 11:52:49 -0700

    s

--- scripts/deploy/iks/run-setup.sh
+++ scripts/deploy/iks/run-setup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#
-# Copyright 2021 kubeflow.org
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 # Remove the x if you need no print out of each command
 set -xe
@@ -26,7 +12,6 @@ REGION=${REGION:-"us-south"}
 ORG=${ORG:-"dev-advo"}
 SPACE=${SPACE:-"dev"}
 RESOURCE_GROUP=${RESOURCE_GROUP:-"default"}
-#MAKE_TARGET=${MAKE_TARGET:-"run-go-unittests"}
 GIT_COMMIT_SHORT=$(git log -n1 --format=format:"%h")
 
 # Git repo cloned at $WORKING_DIR, copy into $ARCHIVE_DIR and
@@ -53,20 +38,3 @@ fi
   echo "RESOURCE_GROUP=${RESOURCE_GROUP}"
 } >> "${ARCHIVE_DIR}/build.properties"
 grep -v -i password "${ARCHIVE_DIR}/build.properties"
-
-#retry() {
-#  local max=$1; shift
-#  local interval=$1; shift
-
-#  until "$@"; do
-#    echo "trying.."
-#    max=$((max-1))
-#    if [[ "$max" -eq 0 ]]; then
-#      return 1
-#    fi
-#    sleep "$interval"
-#  done
-#}
-
-#retry 3 3 ibmcloud login --apikey "${IBM_CLOUD_API_KEY}" --no-region
-#retry 3 3 ibmcloud target -r "$REGION" -o "$ORG" -s "$SPACE" -g "$RESOURCE_GROUP"
